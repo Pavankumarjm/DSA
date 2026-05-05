17b3f5f1be78d1da574a32838c45efcbadd5f814
PavanPiku <mspavankumar683@gmail.com>
2026-05-05 20:46:48 +0530

    twoSum

---
+++ ReverseINT.py
@@ -0,0 +1,15 @@
+def reverse_int(n):
+    sign = -1 if n < 0 else 1
+    n = n * sign 
+    
+    rev = 0
+    while n != 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n = n // 10
+    
+    return sign * rev
+
+
+print(reverse_int(123)) 
+print(reverse_int (-456))  
