e1a7d6e90db1640efe69e38218b2052edc943e93
PavanPiku <mspavankumar683@gmail.com>
2026-06-05 20:21:36 +0530

    Second Largest Element in an Array

---
+++ Array/SecLar.py
@@ -0,0 +1,13 @@
+arr = [10, 45, 23, 89, 12]
+
+largest = arr[0]
+second = arr[0]
+
+for i in arr:
+    if i > largest:
+        second = largest
+        largest = i
+    elif i > second and i != largest:
+        second = i
+
+print("Second Largest:", second)
