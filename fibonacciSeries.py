4209d9fa81e3c01aa20f83681de11f7a94c53f18
PavanPiku <mspavankumar683@gmail.com>
2026-06-15 19:51:10 +0530

    fibonacci series

--- fibonacciSeries.py
+++ fibonacciSeries.py
@@ -1,10 +1,10 @@
-num = 4
-a=0
-b=1
-print(a)
-print(b)
-for i in range(2, num):
-    c=a+b
-    print(c)
-    a=b
-    b=c
+n = int(input("Enter the number "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
