546962649fa8b5aa434d6134284d819705fe23d9
PavanPiku <mspavankumar683@gmail.com>
2026-07-22 18:56:38 +0530

    linear search

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,11 +1,11 @@
-arr=[30,20,10,60,7,90,4]
+arr=[30,20,11,70,7,110,6]
 key=7
 found=False
 
 for i in range(len(arr)):
     if arr[i]==key:
-        print("Found",i)
+        print("Found",i);
         found = True
         break
     if not found:
-        print("not found")
+        print("not found");
