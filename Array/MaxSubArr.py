55a1a99780709db31161c9a65a8410c48c6d1838
PavanPiku <mspavankumar683@gmail.com>
2026-07-28 20:06:12 +0530

    max sub array

--- Array/MaxSubArr.py
+++ Array/MaxSubArr.py
@@ -7,4 +7,4 @@ for i in range(1, len(arr)):
     current_sum = max(arr[i], current_sum + arr[i])
     max_sum = max(max_sum, current_sum)
 
-print("Maximum Subarray Sum:", max_sum)
+print("Maximum Subarray Sum:", max_sum);
