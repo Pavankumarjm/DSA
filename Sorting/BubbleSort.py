2c22d7c45db2bad89bd275c739248bbcde30fa53
PavanPiku <mspavankumar683@gmail.com>
2026-07-30 19:11:58 +0530

    Bubble sort

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,7 +1,7 @@
-arr=[50,20,10,75,23,55,4]
+arr=[50,20,10,75,24,56,4,35]
 n=len(arr)
 for i in range (n):
     for j in range(0,n-i-1):
         if arr[j] > arr[j+1]:
             arr[j],arr[j+1]=arr[j+1],arr[j]
-print(arr) 
+print(arr);
