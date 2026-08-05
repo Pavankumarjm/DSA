9b9101ce60053bf682980e186f1e4fb2d8b796e6
PavanPiku <mspavankumar683@gmail.com>
2026-08-05 19:43:38 +0530

    swap

--- swap.py
+++ swap.py
@@ -1,11 +1,11 @@
-num1=50
-num4=24
+num1=50;
+num4=24;
 print("value before swap:", num1)
 print("value before swap", num2)
 
 temp = num1;
-num1=num2;
-num2=temp;
+num1= num2;
+num2= temp;
 
 print("value after swap", num1);
 print("value after swap", num2);
